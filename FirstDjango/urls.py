dce83c5f30ae482dd90f70260bd9dfbd9ecb2e54
Jiraiya228 <dmescherin@gmail.com>
2023-01-28 13:01:11 +0300

    работа со стилями добавлением картинок

--- FirstDjango/urls.py
+++ FirstDjango/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 from MainApp import views
@@ -8,4 +10,4 @@ urlpatterns = [
     path('items_list/',views.items_list, name="items-list"),
     path('item/<int:id>',views.item_page, name="items-page"),
 
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
