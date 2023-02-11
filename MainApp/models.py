88b19344deec453abf8ce286876c6b3dc3fad6db
Jiraiya228 <dmescherin@gmail.com>
2023-02-11 13:16:22 +0300

    работа со стилями

--- MainApp/models.py
+++ MainApp/models.py
@@ -5,3 +5,4 @@ class Item(models.Model):
     name = models.CharField(max_length=100)
     brand = models.CharField(max_length=100)
     count = models.PositiveIntegerField()
+    descriptions = models.CharField(max_length=255)
